0502bd098449114a1a773a5a8a435b58e54098f7
Roman <roman.pawliw@gmail.com>
2016-09-04 20:18:11 +0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,21 @@
 var socket = io("http://localhost:3000"); 
 
 function createColor() {
-        var r = Math.floor(Math.random() * 257);
-        var g = Math.floor(Math.random() * 257);
-        var b = Math.floor(Math.random() * 257);
-        return "rgb" + "(" + ( r + "," + g + "," + b + ")" );
+    var r = Math.floor(Math.random() * 257);
+    var g = Math.floor(Math.random() * 257);
+    var b = Math.floor(Math.random() * 257);
+    return "rgb" + "(" + ( r + "," + g + "," + b + ")" );
 }
 
 function getRandom() {
-	var width = $(window).width() - 200;
-	socket.emit("getRandom", { number: width });
+    var width = $(window).width() - 200;
+    socket.emit("getRandom", { number: width });
 }
 
 setInterval( function() { getRandom() }, 2000);
 
 socket.on("send", function(data) {
-	$("div.slider").css("background-color", createColor()).animate({
-		left: data.num
-	});
+    $("div.slider").css("background-color", createColor()).animate({
+        left: data.num
+    });
 });
